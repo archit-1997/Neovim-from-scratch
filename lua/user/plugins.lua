de2890b6e65a96e2d58f40c747dc36504d06ed2b
Archit Singh <qarsingh@amazon.com>
2024-01-16 19:52:06 +0530

    Added support for nvim telescope

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -40,10 +40,17 @@ packer.init {
 
 -- Install your plugins here
 return packer.startup(function(use)
+  --************************
   -- My plugins here
   use "wbthomason/packer.nvim" -- Have packer manage itself
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
+
+  --Telescope plugins
+  use "nvim-telescope/telescope.nvim"
+  use 'nvim-telescope/telescope-media-files.nvim'
+
+  --************************
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
