e433586f323b2b1cf8af5d36a926bfb8303c13bc
Archit Singh <qarsingh@amazon.com>
2024-01-16 19:59:45 +0530

    Use gruvbox colorscheme

--- init.lua
+++ init.lua
@@ -5,3 +5,7 @@ require "user.keymaps"
 require "user.plugins"
 require "user.colorscheme"
 require "user.telescope"
+
+--setting colorscheme
+vim.o.background = "dark" -- or "light" for light mode
+vim.cmd([[colorscheme gruvbox]])
