649cf06c9ad48d4815a92c64c1d3b5667c2dcac0
Archit Singh <qarsingh@amazon.com>
2024-01-17 17:01:50 +0530

    Changed mapping for telescope live grep

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -48,7 +48,7 @@ keymap("v", ">", ">gv", opts)
 --Telescope keybindings
 -- keymap("n", "<leader>f", "<cmd>Telescope find_files<cr>", opts)
 keymap("n", "<leader>f", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
-keymap("n", "<c-t>", "<cmd>Telescope live_grep<cr>", opts)
+keymap("n", "<c-g>", "<cmd>Telescope live_grep<cr>", opts)
 
 --Nvim Tree keybindings
 keymap("n", "<leader>e", ":NvimTreeToggle<cr>", opts)
